558a19dfbc85540b74d07a61ac3214cb19ac251f
Sindhukumari Parameswaran <33718216+sindhusweety@users.noreply.github.com>
2021-05-15 22:54:56 -0400

    Update find_short_path_sub_designation.py

--- find_short_path_sub_designation.py
+++ find_short_path_sub_designation.py
@@ -1,1 +1,31 @@
+"""
+shortest path
+"""
+def recursive(N, A, unique_n, unique):
+    if unique_n == N:
+        return unique_n
+    ck_visited = []
+    for i in range(N - unique_n + 1):
+        sub_location = A[i:i + unique_n]
+        visited = {k: False for k in unique}
+        for _l in unique:
+            if _l in sub_location:
+                visited[_l] = True
 
+        not_visited = [visited[k] for k in visited]
+        if False not in not_visited:
+            ck_visited = not_visited
+    if False not in ck_visited and len(ck_visited):
+        return unique_n
+    return recursive(N, A, unique_n+1, unique)
+
+
+def solution(A):
+    unique = []
+    for i in A:
+        if i not in unique:
+            unique.append(i)
+    unique_n = len(unique)
+    return  recursive(len(A), A, unique_n, unique)
+
+print(solution([2, 1, 1, 3]))
